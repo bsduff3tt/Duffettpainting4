b0afa367ed3a67333685036011c06ba5583e10d8
bsduff3tt <97562569+bsduff3tt@users.noreply.github.com>
2024-08-01 12:50:14 -0400

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import CreateArea from "./CreateArea";
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <CreateArea  />
+    </div>
+  );
+}
+
+export default App;
